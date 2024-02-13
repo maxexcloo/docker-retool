dc534f52d7aa0baaac543dfb639a7f354a8a6d5b
Max Schaefer <maxexcloo@users.noreply.github.com>
2024-02-13 13:45:12 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 set timeout -1
 spawn python /app/retool/retool.py --update
 expect "Would you like to download them?" { send "y\r" }
+wait
 spawn python /app/retool/retool.py [lrange $argv 0 end]
 expect "Do you want to download them?" { send "y\r" }
+wait
