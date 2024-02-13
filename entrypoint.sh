34e7f26aaa9ec017cfa748f6a82523dbc0fdb947
Max Schaefer <maxexcloo@users.noreply.github.com>
2024-02-13 13:35:56 +1100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
-python /app/retool/retool.py --update
-python /app/retool/retool.py "$@"
+#!/usr/bin/expect
+set timeout -1
+spawn python /app/retool/retool.py --update
+expect "Would you like to download them?" { send "y\r" }
+spawn python /app/retool/retool.py [lrange $argv 0 end]
+expect "Do you want to download them?" { send "y\r" }
